71e7877e0273210c199aa997babe9cd56028a0f7
Coseenus <dimakosinec409@gmail.com>
2024-09-10 22:06:33 +0300

    Add files via upload

---
+++ a-name-surname-age.py
@@ -0,0 +1,8 @@
+a = "Hello World"
+name = "Dmytro"
+surname = "Kosynets"
+age = 16
+print(f"Value and type a: {type(a)}, {a}")
+print(f"Value and type name: {type(name)}, {name}")
+print(f"Value and type surname: {type(surname)}, {surname}")
+print(f"Value and type age: {type(age)}, {age}")
