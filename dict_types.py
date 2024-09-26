6450a0103f0ceade02120be27d2da36b3102e1b7
Coseenus <dimakosinec409@gmail.com>
2024-09-26 09:47:15 +0300

    Create dict_types.py

---
+++ dict_types.py
@@ -0,0 +1,18 @@
+  dict = {
+    "name": "Max",
+    "age": 30,
+    "address": {
+        "city": "Kivertsi",
+        "street": "5th Avenue",
+        "house_number": 10,
+        "apartment": 6,
+        "zip_code": "45200"},
+     "weight_kg": 121 }
+type_items = {}
+for k, v in dict.items():
+    if type(v) == dict:
+        for k1, v1 in v.items():
+            type_items[k1] = type(v1)
+    else:
+        type_items[k] = type(v)
+print(type_items)
