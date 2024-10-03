18df973239b532562951819bfe71f049f07cbc08
Coseenus <dimakosinec409@gmail.com>
2024-10-03 09:21:48 +0300

    Create uniqNsort.py

---
+++ uniqNsort.py
@@ -0,0 +1,26 @@
+input_list = [3, 1, 2, 3, 4, 5, 6, 3, 4, 5, 7, 6, 5, 4, 3, 4, 5, 4, 3, 'Привіт', 'анаконда']
+
+def remove_dupl(input_list):
+    result = []
+    for item in input_list:
+        if item not in result:
+            result.append(item)
+    return result
+
+def sort_list(unique_list):
+    numbers = []
+    strings = []
+    for i in unique_list:
+        if type(i) == int:
+            numbers.append(i)
+        elif type(i) == str:
+            strings.append(i)
+    numbers.sort()
+    strings.sort()
+    return numbers + strings
+
+unique_list = remove_dupl(input_list)
+sorted_list = sort_list(unique_list)
+
+print("List without duplicates:", unique_list)
+print("List is sorted:", sorted_list )
